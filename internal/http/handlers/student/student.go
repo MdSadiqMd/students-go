400c263340679c8c495d9f292d8100d94d88be1e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-10 18:05:58 +0530

    ⚔️ Response generalised

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,17 +7,18 @@ import (
 	"net/http"
 
 	"github.com/MdSadiqMd/students-go/internal/types"
+	"github.com/MdSadiqMd/students-go/internal/utils/response"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var student types.Student // defining type of incoming datga and holding it in a variable
+		var student types.Student // defining type of incoming data and holding it in a variable
 
 		err := json.NewDecoder(r.Body).Decode(&student) // decoding incoming data
-		if errors.Is(err, io.EOF) { // checking incoming data is type of Student or not
-
+		if errors.Is(err, io.EOF) {                     // checking incoming data is type of Student or not
+			response.WriteJson(w, http.StatusBadRequest, err.Error())
+			return
 		}
-
-		w.Write([]byte("Creating StudentAPI is live 🥳"))
+		response.WriteJson(w, http.StatusCreated, map[string]interface{}{"data": student, "success": "OK"})
 	}
 }
