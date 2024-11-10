0007d7b4e1defd0276d5a47ae474c284c272702b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-10 17:40:55 +0530

    🛟  Parsing request data

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,9 +1,23 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/MdSadiqMd/students-go/internal/types"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var student types.Student // defining type of incoming datga and holding it in a variable
+
+		err := json.NewDecoder(r.Body).Decode(&student) // decoding incoming data
+		if errors.Is(err, io.EOF) { // checking incoming data is type of Student or not
+
+		}
+
 		w.Write([]byte("Creating StudentAPI is live 🥳"))
 	}
 }
